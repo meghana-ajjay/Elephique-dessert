24691e1947917ab4033b293ab0061912af787d8f
meghana-ajjay <meganasa67@gmail.com>
2026-07-01 11:49:03 +0530

    Add Godle Analytics

---
+++ src/Analytics.js
@@ -0,0 +1,5 @@
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-D83P3CPYD3");
+
+export default ReactGA;
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
+import ReactGA from "./Analytics";
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
